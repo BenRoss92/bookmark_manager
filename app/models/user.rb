665c9b7436b5e8e218bc6dc2e3a02f6b19e7c307
BenRoss92 <benjaminjackross@gmail.com>
2016-09-09 11:30:17 +0100

    email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,13 @@ class User
 include DataMapper::Resource
 
 property :id, Serial
-property :email, String
+property :email, String, required: true
 property :password, BCryptHash
 
 attr_reader :password
 attr_accessor :password_confirmation
 
 validates_confirmation_of :password
-
+validates_presence_of :email
+validates_format_of :email, :as => :email_address
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -23,4 +23,24 @@ feature 'let a user sign up' do
     expect(page).to have_content("Passwords do not match")
   end
 
+  scenario 'user cannot sign in without an email address' do
+    visit '/sign_up'
+    fill_in('email', with: '')
+    fill_in('password', with: 'iamjoseph')
+    fill_in('password_confirmation', with: 'iamjoseph')
+    click_button('Sign Up')
+    expect(User.count).to eq(0)
+    expect(page).to have_current_path("/sign_up")
+  end
+
+  scenario 'user cannot sign in with an invalid email address' do
+    visit '/sign_up'
+    fill_in('email', with: 'hello@user')
+    fill_in('password', with: 'iamjoseph')
+    fill_in('password_confirmation', with: 'iamjoseph')
+    click_button('Sign Up')
+    expect(User.count).to eq(0)
+    expect(page).to have_current_path("/sign_up")
+  end
+
 end
